524dd48c7a67bf515cdd04aab1be96eddcfc201f
Hafizul Haque <hafizul.haque@reddotdigitalit.com>
2023-08-03 08:47:04 +0600

    necessary imports from angular-cdk imported and drag-n-drop view created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { UserInterface } from './user.interface';
 import { userData } from './data';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
@@ -8,5 +10,10 @@ import { userData } from './data';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   users: UserInterface[] = userData;
+
+  drop(e: any){
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CdkDropListGroup, CdkDropList, CdkDrag } from '@angular/cdk/drag-drop';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CdkDropListGroup,
+    CdkDropList,
+    CdkDrag
   ],
   providers: [],
   bootstrap: [AppComponent]
